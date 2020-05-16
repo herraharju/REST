7431a7d32269203993e91e6ccbf81c5ca108320b
herraharju <herra-harju@hotmail.com>
2020-05-16 22:07:38 +0300

    First commit with customer testing stuff.

---
+++ Entities/POCO/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Resting.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Resting/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Resting.Models;
+
+namespace Resting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController()
+        {
+            _customerRepo = new CustomerRepo();
+        }
+        
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "Get")]
+        public Customer Get(Guid id) =>_customerRepo.Read(id);
+
+        // POST: api/Customer
+        [HttpPost]
+        public void Post([FromBody] Customer customer) => _customerRepo.Create(customer);
+                
+        // PUT: api/Customer/5
+        [HttpPut("{id}")]
+        public bool Put([FromBody] Customer customer) => new CustomerRepo().Update(customer);
+
+        // DELETE: api/Customer/5
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => _customerRepo.Delete(id);
+
+        private CustomerRepo _customerRepo;
+    }
+}
